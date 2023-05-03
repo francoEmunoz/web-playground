c6e47f2105ff07d84664352fdbb4452881cceebd
francoEmunoz <munozfranco011@gmail.com>
2023-05-02 21:03:18 -0300

    views2

--- project/pages/views.py
+++ project/pages/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Page
+from .forms import PageForm
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PageListView(ListView):
+    model = Page
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+class PageDetailView(DetailView):
+    model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    form_class = PageForm
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+    
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')

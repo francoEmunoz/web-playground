5f1a6214defaa5300771e6292c511af54600277e
francoEmunoz <munozfranco011@gmail.com>
2023-05-05 21:59:27 -0300

    registration urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls'))
 ]

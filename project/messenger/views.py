f8428b2471cb61b092d07d833e1a4c78115dde2e
francoEmunoz <munozfranco011@gmail.com>
2023-05-11 21:37:17 -0300

    views

--- project/messenger/views.py
+++ project/messenger/views.py
@@ -3,7 +3,10 @@ from django.utils.decorators import method_decorator
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from django.http import Http404, JsonResponse
-from .models import Thread
+from .models import Thread, Message
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 # Create your views here.
 @method_decorator(login_required, name="dispatch")
@@ -22,5 +25,20 @@ class ThreadDetail(DetailView):
     
 def add_message(request, pk):
     json_response = {'created':False}
+    if request.user.is_authenticated:
+        content = request.GET.get('content', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)  
+            message = Message.objects.create(user=request.user, content=content)
+            thread.messages.add(message)
+            json_response['created'] = True
+    else:
+        raise Http404("User is not authenticated")
 
     return JsonResponse(json_response)
+
+@login_required
+def start_thread(request, username):
+    user = get_object_or_404(User, username=username)
+    thread = Thread.objects.find_or_create(user, request.user)
+    return redirect(reverse_lazy('messenger:detail', args=[thread.pk]))
